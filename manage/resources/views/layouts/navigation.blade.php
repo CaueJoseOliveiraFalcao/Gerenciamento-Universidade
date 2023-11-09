ee8eb9382a1742ae31dc116bcecb35f505be31b7
Cauê Josê Oliveira Falcão <cauejose007@gmail.com>
2023-11-09 16:54:20 -0400

    y

--- manage/resources/views/layouts/navigation.blade.php
+++ manage/resources/views/layouts/navigation.blade.php
@@ -1,13 +1,14 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }"  style="background-color: black">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
+                <div class="shrink-0 flex  flex-col justify-center  items-center">
+                    <h1 class="text-8xl">FACU-GEB</h1>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+                      </svg>
                 </div>
 
                 <!-- Navigation Links -->
--- manage/routes/web.php
+++ manage/routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 Route::get('/register' , [RegisteredUserController::class , 'create'])->name('showRegisterForm');
 Route::get('/dashboard', function () {
