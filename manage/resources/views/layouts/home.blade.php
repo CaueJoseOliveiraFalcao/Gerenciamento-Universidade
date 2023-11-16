fccb4ada915e335fbc707aa64e481284ed0c65ac
Cauê Josê Oliveira Falcão <cauejose007@gmail.com>
2023-11-16 11:41:59 -0400

    update css

--- manage/resources/views/layouts/home.blade.php
+++ manage/resources/views/layouts/home.blade.php
@@ -45,25 +45,125 @@
                 
 
                 <div>
-                    <a href="/login" class="bg-white   hover:bg-gray-100 text-gray-800 font-semibold py-4 px-6 border border-gray-400 rounded shadow">
+                    <a href="/login" class="bg-white   hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
                         Login
                     </a>
                     
-                    <a href="/register" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-4 px-6 border border-gray-400 rounded shadow">
+                    <a href="/register" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold  py-2 px-4 border border-gray-400 rounded shadow">
                         Register
                     </a>
                 </div>  
             </div>
         </div>
     </nav>   
-    <main style="display: flex;align-items:center;justify-content:space-around;background-color:rgba(28,28,28,255);color:white; flex-wrap;wrap">
+    <main>
+
+    </main>
+    <section class="lading1">
         <div>
             <x-application-logo width="200" height="200" />
         </div>
-        <div>
-            <h1 class="font-extralight">Bem-vindo ao Sistema de Gerenciamento de Matrículas</h1>
+        <div class="divLading">
+            <h1 class="h1Landing">Bem-vindo ao Sistema de Gerenciamento de Matrículas</h1>
+            <p class="pLading">O nosso sistema oferece uma maneira eficiente e 
+                simplificada de gerenciar as matrículas dos alunos.
+                Com uma interface amigável e recursos avançados,
+                proporcionamos uma experiência completa para administradores,
+                professores e alunos.</p>
         </div>
-    </main>
-    
+    </section>
+    <section class="aboutUs">
+        <h1>Sobre Nós</h1>
+        <p>
+            O Sistema de Gerenciamento de Matrículas é uma plataforma abrangente projetada para facilitar a administração educacional,
+            proporcionando uma experiência de usuário eficiente e intuitiva.
+            Este sistema permite que os alunos se matriculem em cursos, verifiquem sua
+            frequência e agendem provas. Ele oferece uma visão clara do progresso 
+            acadêmico do aluno, permitindo que eles acompanhem suas notas, frequência
+            e horários de prova de maneira organizada.
+            <br>
+            <br>
+            Os professores têm a capacidade de gerenciar seus alunos, monitorando a
+            frequência e programando provas. Eles podem acessar informações detalhadas
+            sobre o desempenho do aluno, facilitando a identificação de áreas que precisam
+            de atenção adicional. Além disso, os professores podem comunicar-se diretamente
+            com os alunos através do sistema, proporcionando feedback oportuno e eficaz.
+            <br>
+            <br>
+            Os coordenadores desempenham um papel crucial na criação de turmas, garantindo
+            que os alunos estejam matriculados nos cursos corretos e que os professores 
+            estejam alocados de maneira adequada. Eles têm a capacidade de visualizar e 
+            gerenciar todas as informações do curso, incluindo horários, locais e matrículas de alunos.
+            <br>
+            <br>
+            Em resumo, o Sistema de Gerenciamento de Matrículas é uma ferramenta poderosa que simplificada
+            a administração educacional, melhorando a experiência de todos os envolvidos no processo de
+            aprendizagem. Ele promove a eficiência, a comunicação e a transparência, contribuindo para
+            um ambiente de aprendizagem mais produtivo e enriquecedor.
+        </p>
+    </section>
     </body>
+    <style>
+        body::-webkit-scrollbar {
+            display: none;
+        }
+
+        /*Lading */
+        .h1Landing {
+            font-size:23px;
+        }
+        .pLading{
+            max-width: 500px;
+            margin-top: 1rem
+        }
+        .lading1{
+            display: flex;
+            align-items:center;
+            justify-content:space-around;
+            background-color:rgba(28,28,28,255);
+            color:white; 
+            flex-wrap: wrap;
+            height: 91vh;
+        }
+        @media (max-width : 768px){
+            .lading1{
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+            }
+            .pLading{
+                margin-top: 1rem;
+                text-align: center;
+                font-size: 12px
+            }
+            .h1Landing{
+                text-align: center
+            }
+            .divLading{
+                display:flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+            }
+        }
+        /*About us*/
+        .aboutUs{
+            background-color: #fffec4;
+            height: 100vh;
+            color: black;
+            padding-left:12%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center
+        }
+        .aboutUs h1{
+            font-weight: bold;
+            font-size: 23px;
+
+        }
+        .aboutUs p{
+            max-width: 1000px;
+
+        }
+    </style>
 </html>
