2b5b5111db1ebc84a0d565979f327cac7a54bed7
Cauê Josê Oliveira Falcão <cauejose007@gmail.com>
2024-02-20 12:16:03 -0300

    groups and permission variables 1/2

--- app/app/Http/Controllers/PainelController.php
+++ app/app/Http/Controllers/PainelController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Group;
+use App\Models\Permission;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Symfony\Component\Console\Input\Input;
@@ -14,26 +16,36 @@ class PainelController extends Controller
         $user = session('user');
         $userPermission = $user->permissions->first->first()->permission;
         $isFinded = 'notseached';
-        $users = $this->seachAndGetUsers($request , $user , $userPermission , $isFinded);
-        return view('auth.dashboard.painel.painel' , compact('users' , 'isFinded'));
+        $DisponibleGroups = '';
+        $DisponiblePermissions = '';
+        $users = $this->seachAndGetUsers($request , $user , $userPermission , $isFinded , $DisponibleGroups , $DisponiblePermissions);
+        return view('auth.dashboard.painel.painel' , compact('users' , 'isFinded' , 'DisponibleGroups' , 'DisponiblePermissions'));
     }
-    public function seachAndGetUsers(Request $input , $user , $permission , &$isFinded){
+    public function seachAndGetUsers(Request $input , $user , $permission , &$isFinded , &$DisponibleGroups , &$DisponiblePermissions){
         if($input->input('search') && $permission === 'admin'){
             $FindUser = User::where('email' , $input->input('search'))->first();
             if (($FindUser)){
                 $isFinded = 'notfound';
+                $DisponiblePermissions = Permission::all();
+                $DisponibleGroups = Group::all();
                 return $FindUser;
             }
             else{
                 $isFinded = 'notfound';
+                $DisponiblePermissions = Permission::all();
+                $DisponibleGroups = Group::all();
                 return User::all();
             }
         }
         elseif ($permission === 'admin'){
+            $DisponibleGroups = Group::all();
+            $DisponiblePermissions = Permission::all();
             return $users = User::all();
         }
         elseif ($input->input('search') && $permission === 'coordinator'){
             $user_group_id = $user->group_id;
+            $GroupsAcessArray = [$user_group_id , 7];
+            $PermissionsAcessArray = [$permission , 'usp'];
             $FindUser = User::join('permission_user', 'users.id', '=', 'permission_user.user_id')
                 ->join('permissions', 'permission_user.permission_id', '=', 'permissions.id')
                 ->whereIn('permissions.permission', ['coordinator', 'teacher' , 'student'])
@@ -43,11 +55,15 @@ class PainelController extends Controller
                 ->distinct()
                 ->get();
                 if (count($FindUser) > 0){
+                    $DisponiblePermissions = Permission::whereIn('permission' , $PermissionsAcessArray)->get();
+                    $DisponibleGroups = Group::whereIn('id', $GroupsAcessArray)->get();
                     $isFinded = 'found';
                     return $FindUser;
                 }
                 else{
                     $isFinded = 'notfound';
+                    $DisponibleGroups = Group::whereIn('id', $GroupsAcessArray)->get();
+                    $DisponiblePermissions = Permission::whereIn('permission' , $PermissionsAcessArray)->get();
                     $user_group_id = $user->group_id;
                     return  User::join('permission_user', 'users.id', '=', 'permission_user.user_id')
                     ->join('permissions', 'permission_user.permission_id', '=', 'permissions.id')
@@ -60,6 +76,10 @@ class PainelController extends Controller
         }
         elseif($permission === 'coordinator'){
             $user_group_id = $user->group_id;
+            $GroupsAcessArray = [$user_group_id , 7];
+            $PermissionsAcessArray = [$permission , 'usp'];
+            $DisponibleGroups = Group::whereIn('id', $GroupsAcessArray)->get();
+            $DisponiblePermissions = Permission::whereIn('permission' , $PermissionsAcessArray)->get();
             return  User::join('permission_user', 'users.id', '=', 'permission_user.user_id')
             ->join('permissions', 'permission_user.permission_id', '=', 'permissions.id')
             ->whereIn('permissions.permission', ['coordinator', 'teacher' , 'student'])
--- app/resources/views/auth/dashboard/painel/painel.blade.php
+++ app/resources/views/auth/dashboard/painel/painel.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Painel Administrador</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
     <h1>Painel</h1>
@@ -12,6 +13,107 @@
     @foreach ($users as $user)
         <p>{{ $user->name }}</p>
     @endforeach
+    
+
+<div class=" overflow-x-auto flex flex-col ">
+    <table class=" text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3 text-2xl">
+                    Nome
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Email
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Grupo
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Price
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <p>{{ $user->name }}</p>
+            @foreach ($users as $user)
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{$user->name}}
+                </th>
+                <td class="px-6 py-4">
+                    {{$user->email}}
+                    DisponiblePermissions
+                </td>
+                <td class="px-6 py-4">
+                    <p class="text-sm-2" >Grupo Atual : {{$user->group['name']}} </p>
+                    <select name="group" id="gruop" class="group">
+                    @foreach ($DisponibleGroups as $Group)
+                        <option value="{{$Group->name}}" {{$user->group['name'] == $Group->name ? 'selected' : ''}}>
+                            {{$Group->name}}
+                        </option>
+                    @endforeach
+
+                </td>
+                <td class="px-6 py-4">
+                    @php
+                        $userPermission = $user->permissions->first->first()->permission
+                    @endphp
+                    <p>Permissão Atual : {{$userPermission}} </p>
+                    <select name="permission" id="permission" class="permission">
+                    @foreach ($DisponiblePermissions as $Permission)
+                        <option value="{{$Permission->permission}}" {{$user->group['name'] == $Permission->permission ? 'selected' : ''}}>
+                            {{$Permission->permission}}
+                        </option>
+                    @endforeach
+                </td>
+            </tr>
+        @endforeach
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Apple MacBook Pro 17"
+                </th>
+                <td class="px-6 py-4">
+                    Silver
+                </td>
+                <td class="px-6 py-4">
+                    Laptop
+                </td>
+                <td class="px-6 py-4">
+                    $2999
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Microsoft Surface Pro
+                </th>
+                <td class="px-6 py-4">
+                    White
+                </td>
+                <td class="px-6 py-4">
+                    Laptop PC
+                </td>
+                <td class="px-6 py-4">
+                    $1999
+                </td>
+            </tr>
+            <tr class="bg-white dark:bg-gray-800">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Magic Mouse 2
+                </th>
+                <td class="px-6 py-4">
+                    Black
+                </td>
+                <td class="px-6 py-4">
+                    Accessories
+                </td>
+                <td class="px-6 py-4">
+                    $99
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
 
 
 {{-- Exibir isFinded --}}
