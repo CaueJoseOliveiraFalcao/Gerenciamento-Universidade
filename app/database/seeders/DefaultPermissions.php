e3eef9e0970186a932ce331b638c87e58e7d9740
Cauê Josê Oliveira Falcão <cauejose007@gmail.com>
2024-03-05 10:35:32 -0400

    update

---
+++ app/database/seeders/DefaultPermissions.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DefaultPermissions extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $defaultPermissions = ['admin' , 'coordinator' , 'teacher' , 'sudent'];
+
+
+    }
+}
